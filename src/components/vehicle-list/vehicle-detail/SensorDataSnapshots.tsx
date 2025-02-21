bfc67675335cade36398c4376bae606c6496a124
sahilauspicioussoft <sahil@auspicioussoft.com>
2025-02-21 17:04:25 +0530

    Refactor SensorDataSnapshots component to improve layout and add functionality for displaying sensor data snapshots

--- src/components/vehicle-list/vehicle-detail/SensorDataSnapshots.tsx
+++ src/components/vehicle-list/vehicle-detail/SensorDataSnapshots.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { Plus } from "lucide-react";
 
-const SensorDataSnapshots = () => {
+export default function Snapshots() {
   return (
-    <div>
-      Sensor
+    <div className="col-span-12 flex   h-screen bg-[#171717] text-white">
+      {/* Sidebar */}
+      <div className="w-full md:w-1/3 h-full p-4 md:border-b-0 border-r border-gray-700 flex flex-col">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-lg font-semibold">Snapshots</h2>
+          <button className="p-1 rounded  hover:bg-gray-700">
+            <Plus className="w-5 h-5" />
+          </button>
+        </div>
+        <p className="text-gray-400">No Sensor Data Snapshots Found</p>
+      </div>
+      
+      {/* Main content */}
+      <div className="flex-1 flex items-center justify-center p-4">
+        <p className="text-gray-400 text-center">Select Service Data Snapshot to View</p>
+      </div>
     </div>
-  )
+  );
 }
-
-export default SensorDataSnapshots
